fc84cbc56c31506585d6aa45978b5a9718684766
Tjandra Putra <tjandrap.work@gmail.com>
2025-08-12 15:40:01 +0800

    Update upstash.js

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -8,7 +8,7 @@ dotenv.config();
 // allows 5 requests every 10 seconds
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(10, "30 s"), // 2 requests every 30 seconds
+  limiter: Ratelimit.slidingWindow(20, "30 s"), // 2 requests every 30 seconds
 });
 
 export default ratelimit;
